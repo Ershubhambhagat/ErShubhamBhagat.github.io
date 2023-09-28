15d718ade36c556972fe91580aa1c0f7746d43ad
Shubham Kumar <2090944@cognizant.com>
2023-09-28 18:46:26 +0530

    Path changes

--- Blog/JS/Blog.js
+++ Blog/JS/Blog.js
@@ -3,7 +3,7 @@
 
 function fetchAndPopulateBlogPost() {
 
-    fetch('Json\BlogPost.json') // Adjust the path to your JSON file
+    fetch('Blog/Json/BlogPost.json') // Adjust the path to your JSON file
 
         .then(response => {
 
